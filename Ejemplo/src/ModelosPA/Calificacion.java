c0de2bb8f2d63ed09fd8aa2e1728e13a282cbb10
lucas <rotondarolucas@gmail.com>
2020-10-03 18:00:05 -0300

    calificacion

---
+++ Ejemplo/src/ModelosPA/Calificacion.java
@@ -0,0 +1,47 @@
+package ModelosPA;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="calificacion")
+public class Calificacion {
+    
+    @Id @GeneratedValue(strategy=javax.persistence.GenerationType.AUTO)
+    private long id;
+       
+    @Column(columnDefinition = "TEXT")
+    private String numero;       
+   
+    @Column(columnDefinition = "TEXT")
+    private String fecha;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    
+}
