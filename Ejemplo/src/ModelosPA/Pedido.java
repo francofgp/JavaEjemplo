799602c4fa2122688e2149e4d101907c73d8fe74
crapboy <giulianopertile@gmail.com>
2020-11-15 17:44:58 -0300

    cambie de oneToOne a ManyToMany en atributo producto de pedidos, ahora anda chanta

--- Ejemplo/src/ModelosPA/Pedido.java
+++ Ejemplo/src/ModelosPA/Pedido.java
@@ -7,7 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -38,7 +40,8 @@ public class Pedido {
     private Comercio comercio;
     
     //@ManyToMany(targetEntity = DetallePedido.class, cascade= CascadeType.ALL, fetch=FetchType.LAZY )
-    @OneToMany
+    
+    @ManyToMany
     private List<Producto> producto;
     
     @Column(columnDefinition = "TEXT")
